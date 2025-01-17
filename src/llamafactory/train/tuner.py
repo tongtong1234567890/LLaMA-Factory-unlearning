c48755cfc0cc82f373605518317c4ca98df526c8
tongtong1234567890 <137031851+tongtong1234567890@users.noreply.github.com>
2025-01-18 00:15:39 +0800

    Update tuner.py

--- src/llamafactory/train/tuner.py
+++ src/llamafactory/train/tuner.py
@@ -12,162 +12,136 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import shutil
 from typing import TYPE_CHECKING, Any, Dict, List, Optional
 
 import torch
 from transformers import PreTrainedModel
 
+from .forget import run_forget
 from ..data import get_template_and_fix_tokenizer
-from ..extras import logging
-from ..extras.constants import V_HEAD_SAFE_WEIGHTS_NAME, V_HEAD_WEIGHTS_NAME
-from ..extras.packages import is_ray_available
-from ..hparams import get_infer_args, get_ray_args, get_train_args, read_args
-from ..model import load_model, load_tokenizer
-from .callbacks import LogCallback, PissaConvertCallback, ReporterCallback
+from ..extras.callbacks import LogCallback, LLMWandbCallback
+from ..extras.logging import get_logger
+from ..hparams import get_infer_args, get_train_args
+from ..model import load_model_and_tokenizer
 from .dpo import run_dpo
-from .kto import run_kto
 from .ppo import run_ppo
 from .pt import run_pt
 from .rm import run_rm
 from .sft import run_sft
-from .trainer_utils import get_ray_trainer, get_swanlab_callback
-
-
-if is_ray_available():
-    from ray.train.huggingface.transformers import RayTrainReportCallback
 
 
 if TYPE_CHECKING:
     from transformers import TrainerCallback
 
 
-logger = logging.get_logger(__name__)
+logger = get_logger(__name__)
 
 
-def _training_function(config: Dict[str, Any]) -> None:
-    args = config.get("args")
-    callbacks: List[Any] = config.get("callbacks")
-    model_args, data_args, training_args, finetuning_args, generating_args = get_train_args(args)
+def run_exp(
+    args: Optional[Dict[str, Any]] = None,
+    callbacks: Optional[List["TrainerCallback"]] = None,
+):
+    model_args, data_args, training_args, finetuning_args, generating_args = (
+        get_train_args(args)
+    )
+    callbacks = [LogCallback()] if callbacks is None else callbacks
 
-    callbacks.append(LogCallback())
-    if finetuning_args.pissa_convert:
-        callbacks.append(PissaConvertCallback())
-
-    if finetuning_args.use_swanlab:
-        callbacks.append(get_swanlab_callback(finetuning_args))
-
-    callbacks.append(ReporterCallback(model_args, data_args, finetuning_args, generating_args))  # add to last
+    if training_args.use_wandb:
+        config_dict = {
+            "model": model_args.to_dict(),
+            "data": data_args.to_dict(),
+            "training": training_args.to_dict(),
+            "finetuning": finetuning_args.to_dict(),
+            "generating": generating_args.to_dict(),
+        }
+        callbacks.append(LLMWandbCallback(config_dict=config_dict))
 
     if finetuning_args.stage == "pt":
         run_pt(model_args, data_args, training_args, finetuning_args, callbacks)
     elif finetuning_args.stage == "sft":
-        run_sft(model_args, data_args, training_args, finetuning_args, generating_args, callbacks)
+        run_sft(
+            model_args,
+            data_args,
+            training_args,
+            finetuning_args,
+            generating_args,
+            callbacks,
+        )
     elif finetuning_args.stage == "rm":
         run_rm(model_args, data_args, training_args, finetuning_args, callbacks)
     elif finetuning_args.stage == "ppo":
-        run_ppo(model_args, data_args, training_args, finetuning_args, generating_args, callbacks)
+        run_ppo(
+            model_args,
+            data_args,
+            training_args,
+            finetuning_args,
+            generating_args,
+            callbacks,
+        )
     elif finetuning_args.stage == "dpo":
         run_dpo(model_args, data_args, training_args, finetuning_args, callbacks)
-    elif finetuning_args.stage == "kto":
-        run_kto(model_args, data_args, training_args, finetuning_args, callbacks)
+    elif finetuning_args.stage == "forget":
+        run_forget(model_args, data_args, training_args, finetuning_args, generating_args, callbacks)
     else:
-        raise ValueError(f"Unknown task: {finetuning_args.stage}.")
+        raise ValueError("Unknown task.")
 
 
-def run_exp(args: Optional[Dict[str, Any]] = None, callbacks: Optional[List["TrainerCallback"]] = None) -> None:
-    args = read_args(args)
-    ray_args = get_ray_args(args)
-    callbacks = callbacks or []
-    if ray_args.use_ray:
-        callbacks.append(RayTrainReportCallback())
-        trainer = get_ray_trainer(
-            training_function=_training_function,
-            train_loop_config={"args": args, "callbacks": callbacks},
-            ray_args=ray_args,
-        )
-        trainer.fit()
-    else:
-        _training_function(config={"args": args, "callbacks": callbacks})
-
-
-def export_model(args: Optional[Dict[str, Any]] = None) -> None:
+def export_model(args: Optional[Dict[str, Any]] = None):
     model_args, data_args, finetuning_args, _ = get_infer_args(args)
 
     if model_args.export_dir is None:
-        raise ValueError("Please specify `export_dir` to save model.")
+        raise ValueError("Please specify `export_dir`.")
 
-    if model_args.adapter_name_or_path is not None and model_args.export_quantization_bit is not None:
+    if (
+        model_args.adapter_name_or_path is not None
+        and model_args.export_quantization_bit is not None
+    ):
         raise ValueError("Please merge adapters before quantizing the model.")
 
-    tokenizer_module = load_tokenizer(model_args)
-    tokenizer = tokenizer_module["tokenizer"]
-    processor = tokenizer_module["processor"]
-    get_template_and_fix_tokenizer(tokenizer, data_args)
-    model = load_model(tokenizer, model_args, finetuning_args)  # must after fixing tokenizer to resize vocab
+    model, tokenizer = load_model_and_tokenizer(model_args, finetuning_args)
+    get_template_and_fix_tokenizer(tokenizer, data_args.template)
 
-    if getattr(model, "quantization_method", None) is not None and model_args.adapter_name_or_path is not None:
+    if (
+        getattr(model, "quantization_method", None)
+        and model_args.adapter_name_or_path is not None
+    ):
         raise ValueError("Cannot merge adapters to a quantized model.")
 
     if not isinstance(model, PreTrainedModel):
         raise ValueError("The model is not a `PreTrainedModel`, export aborted.")
 
-    if getattr(model, "quantization_method", None) is not None:  # quantized model adopts float16 type
-        setattr(model.config, "torch_dtype", torch.float16)
+    if getattr(model, "quantization_method", None):
+        model = model.to("cpu")
+    elif hasattr(model.config, "torch_dtype"):
+        model = model.to(getattr(model.config, "torch_dtype")).to("cpu")
     else:
-        if model_args.infer_dtype == "auto":
-            output_dtype = getattr(model.config, "torch_dtype", torch.float16)
-        else:
-            output_dtype = getattr(torch, model_args.infer_dtype)
-
-        setattr(model.config, "torch_dtype", output_dtype)
-        model = model.to(output_dtype)
-        logger.info_rank0(f"Convert model dtype to: {output_dtype}.")
+        model = model.to(torch.float16).to("cpu")
+        setattr(model.config, "torch_dtype", torch.float16)
 
     model.save_pretrained(
         save_directory=model_args.export_dir,
-        max_shard_size=f"{model_args.export_size}GB",
+        max_shard_size="{}GB".format(model_args.export_size),
         safe_serialization=(not model_args.export_legacy_format),
     )
     if model_args.export_hub_model_id is not None:
         model.push_to_hub(
             model_args.export_hub_model_id,
             token=model_args.hf_hub_token,
-            max_shard_size=f"{model_args.export_size}GB",
+            max_shard_size="{}GB".format(model_args.export_size),
             safe_serialization=(not model_args.export_legacy_format),
         )
-
-    if finetuning_args.stage == "rm":
-        if model_args.adapter_name_or_path is not None:
-            vhead_path = model_args.adapter_name_or_path[-1]
-        else:
-            vhead_path = model_args.model_name_or_path
-
-        if os.path.exists(os.path.join(vhead_path, V_HEAD_SAFE_WEIGHTS_NAME)):
-            shutil.copy(
-                os.path.join(vhead_path, V_HEAD_SAFE_WEIGHTS_NAME),
-                os.path.join(model_args.export_dir, V_HEAD_SAFE_WEIGHTS_NAME),
-            )
-            logger.info_rank0(f"Copied valuehead to {model_args.export_dir}.")
-        elif os.path.exists(os.path.join(vhead_path, V_HEAD_WEIGHTS_NAME)):
-            shutil.copy(
-                os.path.join(vhead_path, V_HEAD_WEIGHTS_NAME),
-                os.path.join(model_args.export_dir, V_HEAD_WEIGHTS_NAME),
-            )
-            logger.info_rank0(f"Copied valuehead to {model_args.export_dir}.")
 
     try:
         tokenizer.padding_side = "left"  # restore padding side
         tokenizer.init_kwargs["padding_side"] = "left"
         tokenizer.save_pretrained(model_args.export_dir)
         if model_args.export_hub_model_id is not None:
-            tokenizer.push_to_hub(model_args.export_hub_model_id, token=model_args.hf_hub_token)
+            tokenizer.push_to_hub(
+                model_args.export_hub_model_id, token=model_args.hf_hub_token
+            )
+    except Exception:
+        logger.warning("Cannot save tokenizer, please copy the files manually.")
 
-        if processor is not None:
-            processor.save_pretrained(model_args.export_dir)
-            if model_args.export_hub_model_id is not None:
-                processor.push_to_hub(model_args.export_hub_model_id, token=model_args.hf_hub_token)
 
-    except Exception as e:
-        logger.warning_rank0(f"Cannot save tokenizer, please copy the files manually: {e}.")
+if __name__ == "__main__":
+    run_exp()
